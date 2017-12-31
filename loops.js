da21b3980d2f25cee2827db770eb87089ce26fa8
Tim Hillhouse <thillhouse2@gmail.com>
2017-12-31 15:10:59 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,7 +18,7 @@ function doWhileLoop(array){
   }
   
   do {
-    array.pop
-  } while array.length > 0 && maybeTrue();
+    array.pop();
+  } while (array.length > 0 && maybeTrue());
   return array;
 }
