4a95105be2237feefdf1a7c40363b657ef45092a
Tim Hillhouse <thillhouse2@gmail.com>
2017-12-31 15:07:00 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,5 +2,12 @@ function forLoop(array){
   for (let i = 0; i < 25; i++){
     array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`);
   }
-  return array
+  return array;
+}
+
+function whileLoop(i){
+  while (i > 0){
+    console.log(i--);
+  }
+  return 'done'
 }
