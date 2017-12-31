90a5848f7f1906636f92cb2e8a4c1cf631c8c0f7
Tim Hillhouse <thillhouse2@gmail.com>
2017-12-31 14:51:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array){
   for (let i = 0; i < 25; i++){
-    array[i] = `I am ${i} strange loop${i === 0 ? \'\' : \'s\'}.`;
+    array[i] = `I am ${i} strange loop${i === 0 ? '' : 's'}.`;
     console.log(array[i]);
     console.log(array.length)
   }
