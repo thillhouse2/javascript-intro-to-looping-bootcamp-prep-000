b8a8213792c22f12e74f20602d392a26edb2eb4a
Tim Hillhouse <thillhouse2@gmail.com>
2017-12-31 14:48:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array){
-  for (let i = 0; i <= 25; i++){
+  for (let i = 0; i < 25; i++){
     array[i] = `I am ${i} strange loop.`;
     console.log(array[i]);
     console.log(array.length)
